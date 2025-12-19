9482867322bcefca0829d6f521e8fde6ea63f35d
mopy <mopy7@users.noreply.github.com>
2025-12-19 09:33:50 +0000

    Add list and detail views for notes

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import NoteListViewHome, NoteListView, NoteDetailView
 
 
 urlpatterns = [
-  path("", views.testing),
+  path("", NoteListViewHome.as_view(), name="note-list-home"),
+  path("notes/", NoteListView.as_view(), name="note-list"),
+  path("notes/<slug>/", NoteDetailView.as_view(), name="note-detail"),
 ]
